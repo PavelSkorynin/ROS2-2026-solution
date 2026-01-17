dc43948fd52a4109094832ff41e1bc9730a63363
Pavel Skorynin <pavel.skorynin@yandex.ru>
2026-01-17 10:16:51 +0500

    базовая реализация через micro-ros

---
+++ source/LowBoardController/include/wifi_config.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// Настройте здесь ваши данные WiFi
+// Скопируйте этот файл в wifi_config.h и заполните своими данными
+static char WIFI_SSID[] = "family";
+static char WIFI_PASS[] = "fljvfkexit";
+
+
